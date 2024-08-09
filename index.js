2b281e3c899623b285516c5d04f73a435640ca43
Elitezen <dev.alejandromarin@gmail.com>
2024-08-09 11:50:07 -0400

    Updated dependencies

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const { fortune } = require('./routes/fortune');
-const { fortunes } = require('./routes/fortunes');
-const { home } = require('./routes/home');
+const { fortune } = require('./src/routes/fortune');
+const { fortunes } = require('./src/routes/fortunes');
+const { home } = require('./src/routes/home');
 
 app.use(cors());
 app.use(express.json());
-app.listen(PORT, () => console.log(`Ready on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
 
 app.get('/', home); 
 app.get('/fortune', fortune);
